e317a5cf685948d4badd430b06ced412ac198dc7
Zsolt Szarka <zszarka@dundee.ac.uk>
2017-01-15 00:24:03 +0000

    before adding session service

--- flask_catalog_login/NginxVideo-master/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop nserv
-docker rm nserv
-docker build .
-docker run -d -p 1935:1935 -p 80:80 --name nserv -v $PWD/mp4:/var/mp4s -v $PWD/www:/var/www nvid /usr/local/nginx-streaming/sbin/nginx
-docker start nserv
--- flask_catalog_login/session/start.sh
+++
@@ -1,8 +0,0 @@
-docker stop session_web_1
-docker stop session_db_1
-
-docker rm session_web_1
-docker rm session_db_1
-
-docker-compose build
-docker-compose up
